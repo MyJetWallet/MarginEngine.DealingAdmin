94720acbe61014a0a4463498c97b0add0dc0bc2b
Vadim K <vadim.k@smplt.net>
2022-02-04 10:48:17 +0200

    added Applivation Insights

--- DealingAdmin/Program.cs
+++ DealingAdmin/Program.cs
@@ -13,7 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
 var liveDemoManager = new LiveDemoServiceMapper();
 var settingsModel = SettingsReader.ReadSettings<SettingsModel>();
 
-// Add services to the container.
+// Add services to the container
+builder.Services.AddApplicationInsightsTelemetry();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
@@ -29,7 +30,7 @@ builder.Services.BindServices(settingsModel);
 builder.Services.AddAntDesign();
 builder.Services.AddMudServices(config =>
 {
-    config.SnackbarConfiguration.PositionClass = MudBlazor.Defaults.Classes.Position.TopRight;
+    config.SnackbarConfiguration.PositionClass = MudBlazor.Defaults.Classes.Position.BottomRight;
     config.SnackbarConfiguration.PreventDuplicates = false;
     config.SnackbarConfiguration.NewestOnTop = false;
     config.SnackbarConfiguration.ShowCloseIcon = true;
@@ -38,7 +39,6 @@ builder.Services.AddMudServices(config =>
     config.SnackbarConfiguration.ShowTransitionDuration = 500;
     config.SnackbarConfiguration.SnackbarVariant = MudBlazor.Variant.Filled;
 });
-
 
 var app = builder.Build();
 
